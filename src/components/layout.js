91351144fde2b5a379f336ffc0be775c5355f175
PedroEdu6786 <pedrcg835@gmail.com>
2020-08-29 14:10:15 -0500

    feat: auth dependencies installed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
+import { identityContentProvider } from "react-netlify-identity-widget";
 
 import "./layout.css";
 
 function Layout({ children }) {
     return (
-        <>
+        <identityContentProvider url="https://jam-intro-auth.netlify.com">
             <header>
                 <Link to="/">JAMstack app</Link>
             </header>
             <main>{children}</main>
-        </>
+        </identityContentProvider>
     );
 }
 
